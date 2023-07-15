85addd50188caabec148b23a08e79db8ff467d5b
Reahana <reahana233@gmail.com>
2023-07-15 13:48:53 +0600

    blog section

--- src/Components/Blogs/Blogs.jsx
+++ src/Components/Blogs/Blogs.jsx
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Blogs.css'
+import Post from '../Post/Post';
 
 const Blogs = () => {
+    const [posts,setPosts] = useState([]);
+
+    useEffect(()=>{
+        fetch('posts.json')
+        .then(res=> res.json())
+        .then(data => setPosts(data))
+    },[])
+
     return (
         <div className='blogs-container'>
-            <h1>Blogs</h1>
+            <div className="posts-container">
+                {
+                    posts.map( post => <Post 
+                        key = {post.id}
+                        post = {post}></Post>)
+                }
+                
+            </div>
+            <div>
+                <h1>Bookmark</h1>
+            </div>
         </div>
     );
 };
