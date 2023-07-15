d1cc38931584b47fd69dd1942df45d289d9abd75
Reahana <reahana233@gmail.com>
2023-07-16 00:27:30 +0600

    add font awsome

--- src/Components/Post/Post.jsx
+++ src/Components/Post/Post.jsx
@@ -1,25 +1,30 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import './Post.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBookmark } from '@fortawesome/free-solid-svg-icons'
 
 const Post = (props) => {
     const {title,author,publishDate,readTime,img,coverImg} = props.post;
     return (
         <div className='post'>
-            <img src={coverImg} alt="" />
+            <img className='cover' src={coverImg} alt="" />
             <div className="upper-section">
-            <div className="user">
-                <img src={img} alt="" />
-                <div className="user-info">
-                    <h3>{author}</h3>
-                    <p>{publishDate}</p>
+                <div className="user">
+                    <img src={img} alt="" />
+                    <div className="user-info">
+                        <h3>{author}</h3>
+                        <p>{publishDate}</p>
+                    </div>
                 </div>
-            </div>
-            <h4>{readTime} min read</h4>
+                <p>{readTime} min read <FontAwesomeIcon icon={faBookmark}></FontAwesomeIcon></p>
             </div>
             
             <div className="post-info">
                 <h2>{title}</h2>
-                <p>#beginners</p>
+                <p>#beginners #programming</p>
+                <a href="">Mark as read</a>
             </div>
             
         </div>
