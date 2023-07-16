18d6f283fe43ac55ecf046f689f3564bf710e398
Reahana <reahana233@gmail.com>
2023-07-16 12:40:56 +0600

    create sidebar ui

---
+++ src/Components/Bookmark/Bookmark.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import './Bookmark.css'
+const Bookmark = () => {
+    return (
+        <div className="sidebar">
+                <div className="spendTime">
+                    <h3>Spent time on read : </h3>
+                </div>
+                <div className="bookmark-container">
+                <h3>Bookmarked Blogs : </h3>
+                </div>
+                
+        </div>
+    );
+};
+
+export default Bookmark;
