0854c44678becf690ecb466773dcddc81f27ffb1
Reahana <reahana233@gmail.com>
2023-07-19 12:58:30 +0600

    bookmark

--- src/Components/Bookmark/Bookmark.jsx
+++ src/Components/Bookmark/Bookmark.jsx
@@ -5,28 +5,27 @@ import React from 'react';
 import './Bookmark.css'
 
 const Bookmark = (props) => {
-   
-    const bookmarked = props.bookmarked;
+   const spendTime = props.spendTime
+   const bookmarkedBlog = props.bookmarkedBlog
    
     let totalTime = 0 
-    let quantity = 0;
+    
+   for (const post of spendTime) {
+    totalTime = totalTime + post.readTime; 
+    }
+   
+    let totalBookmarkedBlogs = 0
     let title = '';
 
-   for (const post of bookmarked) {
-    if(post.quantity === 0){
-             post.quantity = 1;
-             
-         }
-         
-         post.quantity = post.quantity || 1;
-    totalTime = totalTime + post.readTime;
-    quantity = quantity + post.quantity;
-     
-
-   }
-    
-   
-    // let bookmarkedBlog = 0
+     for (const post of bookmarkedBlog){
+         if(post.totalBookmarkedBlogs === 0){
+             post.totalBookmarkedBlogs = 1;
+             title = post.title;
+        }
+      post.totalBookmarkedBlogs = post.totalBookmarkedBlogs || 1;
+      title = post.title;
+        totalBookmarkedBlogs = totalBookmarkedBlogs + post.totalBookmarkedBlogs;
+     }
     
     return (
         <div className="sidebar">
@@ -34,7 +33,7 @@ const Bookmark = (props) => {
                     <h3>Spent time on read : {totalTime}  </h3>
                 </div>
                 <div className="bookmark-container">
-                <h3>Bookmarked Blogs : {quantity} </h3>
+                <h3>Bookmarked Blogs : {totalBookmarkedBlogs} </h3>
                 <h3> {title} </h3>
                 </div>
                 
